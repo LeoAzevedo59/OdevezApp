147f25465bd2aaf5f0ff337f303cc727f273288f
Leo Azevedo <leofrancoazevedo@gmail.com>
2022-10-23 21:48:17 -0300

    Correção tela configuração

--- src/pages/Configuracao/index.js
+++ src/pages/Configuracao/index.js
@@ -2,9 +2,6 @@ import React from 'react';
 import { View, Text } from 'react-native';
 
 export default function Configuracao() {
-    
-    const navigation = useNavigation();
-
     return (
         <View>
             <Text> Configuracao </Text>
