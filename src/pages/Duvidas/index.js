5938d641fdad316c7d32abeb334b13aeeaf3efaa
Leo Azevedo <leofrancoazevedo@gmail.com>
2022-11-03 21:10:20 -0300

    Componente perguntas e respostas

--- src/pages/Duvidas/index.js
+++ src/pages/Duvidas/index.js
@@ -1,10 +1,49 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, TouchableOpacity } from 'react-native';
+import {
+    Background,
+    ContainerDuvida,
+    Titulo,
+    Descricao,
+    Header
+} from './styles';
+import { AntDesign } from '@expo/vector-icons';
 
 export default function Duvidas() {
+    function AlterarIcone() {
+        if (icone == 'right') {
+            setIcone('down')
+            setExibirDescricao('flex')
+        }
+        else {
+            setIcone('right')
+            setExibirDescricao('none')
+        }
+    }
+
+    const [icone, setIcone] = useState('right');
+    const [exibirDescricao, setExibirDescricao] = useState('none');
+
     return (
-        <View>
-            <Text> Duvidas </Text>
-        </View>
+        <Background>
+            <ContainerDuvida>
+                <Header>
+                    <Titulo>Como alterar a forma de pagamento da assinatura?</Titulo>
+                    <AntDesign name={icone} size={24} color="black" onPress={AlterarIcone} />
+                </Header>
+                <Descricao style={{ display: exibirDescricao }}>Se você fez a assinatura do Odevez Finance pela Web e deseja alterar a forma de pagamento, siga os passos abaixo:{"\n"}{"\n"}
+                    1. mAcesse seu gmail e busque pela palavra chave Odevez Finance Labs. {"\n"}{"\n"}
+                    2. Você encontrará um e-mail do Pagar.me. Acesse ele e então clique em Gerenciar assinatura; {"\n"}{"\n"}
+                    3. Ao clicar, será apresentada uma tela com os detalhes da assinatura e poderá alterar a forma de pagamento.</Descricao>
+            </ContainerDuvida>
+
+            <ContainerDuvida>
+                <Header>
+                    <Titulo>Há uma cobrança que não reconheço?</Titulo>
+                    <AntDesign name={icone} size={24} color="black" onPress={AlterarIcone} />
+                </Header>
+                <Descricao style={{ display: exibirDescricao }}>Ao verificar sua fatura de cartão de crédito, se deparou com uma cobrança que não conhece sua origem ou não sabia que haveria renovação automática? Verifique as informações abaixo e, se comprovar que a cobraça é realmente do Odevez Finance, entre em contato com nossa equipe em odevezfinance.me/contatosuporte.</Descricao>
+            </ContainerDuvida>
+        </Background>
     );
 }
---
+++ src/pages/Duvidas/styles.js
@@ -0,0 +1,28 @@
+import styled from 'styled-components/native';
+
+export const Background = styled.View` 
+flex:1;
+background-color: #F3F3F3;
+padding: 16px;
+`;
+
+export const ContainerDuvida = styled.View`
+margin-bottom: 16px;
+`;
+
+export const Header = styled.View`
+flex-direction: row;
+justify-content: space-between;
+align-items: center;
+margin-bottom: 4px;
+`;
+
+export const Titulo = styled.Text`
+font-size: 16;
+font-weight: bold;
+`;
+
+export const Descricao = styled.Text`
+display: flex;
+text-align: justify;
+`;
